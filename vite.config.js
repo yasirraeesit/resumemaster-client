8bb6aff75adcf09eb3eebc2e5c96cd0a49cb1f1a
Yasir Raees <146189016+yasirraeesit@users.noreply.github.com>
2026-06-28 03:03:51 +0500

    chore: adjust build base path environments for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/resumemaster-client/' : '/',
+  base: process.env.DEPLOY_TARGET === 'gh-pages' ? '/resumemaster-client/' : '/',
 })
 
